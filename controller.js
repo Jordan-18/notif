523d063f0516204a92445e87c74849b23cc3a725
Jordan-18 <jordanistiqlal@unida.gontor.ac.id>
2021-03-05 12:47:39 +0700

    Mengambil data dari sql

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     respone.ok("Aplikasi Res Api Bejalan",res)
 }
+
+// menampilkan semua data 
+exports.getallnotif = function(req,res){
+    connection.query('SELECT * FROM notif', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else{
+            respone.ok(rows, res)
+        }
+    });
+};
